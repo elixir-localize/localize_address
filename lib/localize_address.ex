493e4d3861bd79f4234b33c751e667dfffcfb630
Kip Cole <kipcole9@gmail.com>
2026-04-04 17:31:41 +0100

    Initial commit

---
+++ lib/localize_address.ex
@@ -0,0 +1,18 @@
+defmodule LocalizeAddress do
+  @moduledoc """
+  Documentation for `LocalizeAddress`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LocalizeAddress.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/localize_address_test.exs
@@ -0,0 +1,8 @@
+defmodule LocalizeAddressTest do
+  use ExUnit.Case
+  doctest LocalizeAddress
+
+  test "greets the world" do
+    assert LocalizeAddress.hello() == :world
+  end
+end
